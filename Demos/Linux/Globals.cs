5570c570b11e3ce6c5319ba76767dc7ca8c967de
radaiko <a.radlingmayr@icloud.com>
2026-02-14 00:53:26 +0100

    Add Demos.Linux SDL2 demo application for Linux x64

---
+++ Demos/Linux/Globals.cs
@@ -0,0 +1,9 @@
+// ────── ╔╗                                                                            DEMOS.LINUX
+// ╔═╦╦═╦╦╬╣ Globals.cs
+// ║║║║╬║╔╣║ Global usings for the Demos.Linux project
+// ╚╩═╩═╩╝╚╝ ───────────────────────────────────────────────────────────────────────────────────────
+global using System;
+global using System.Collections.Generic;
+global using System.Runtime.InteropServices;
+global using Silk.NET.WebGPU;
+global using Silk.NET.Core.Native;
